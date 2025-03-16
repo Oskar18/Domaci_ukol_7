1b5bd77926a7edec32a15b786a2b9827dcea8d2c
Oskar18 <775334585@seznam.cz>
2025-03-16 11:41:00 +0100

    seznamy

---
+++ porovnej seznamy.py
@@ -0,0 +1,3 @@
+lines1:(Jablko je červené, Banán je žlutý, Hruška je zelená, Meruňka je oranžová, Třešeň je červená)
+Lines2:(Jablko je červené, Banán je žlutý, Hruška je modrá, Meruňka je oranžová, Třešeň je fialová)
+
